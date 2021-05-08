cb4d13d373e74c9fcd75d8d03f5d37d4a8b3bf19
Kristina Kovach <kristina.kovach@team.wrike.com>
2021-05-08 20:03:25 +0300

    utils - add function arguments

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,12 +1,15 @@
 import getRandomNumber from '../utils.js';
 
+const minNumber = 0;
+const maxNumber = 10000;
+
 const isEven = (number) => number % 2 === 0;
 const setQuestion = (number) => `Question: ${number}`;
 const getCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
 
 const setGameEvenData = () => {
   const gameRules = 'Answer "yes" if the number is even, otherwise answer "no"';
-  const generatedNumber = getRandomNumber();
+  const generatedNumber = getRandomNumber(minNumber, maxNumber);
   const question = setQuestion(generatedNumber);
   const correctAnswer = getCorrectAnswer(generatedNumber);
 
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,3 @@
-const getRandomNumber = () => {
-  const min = 0;
-  const max = 10000;
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export default getRandomNumber;
