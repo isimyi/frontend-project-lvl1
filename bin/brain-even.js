95b64e555d34a9dbee6b1a9304f7fa0f3608d606
Kristina Kovach <kristina.kovach@team.wrike.com>
2021-05-08 20:00:56 +0300

    brain-even: fix shebang

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
-#!/Users/kristina.kovach/.nvm/versions/node/v16.1.0/bin/node
+#!/usr/bin/env nodе
 import playGame from '../src/index.js';
 import setGameEvenData from '../src/games/game-even.js';
 
