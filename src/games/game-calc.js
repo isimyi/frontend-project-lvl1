744c7072ea9587e791d4d4f40b5a18fe5582a35a
Kristina Kovach <kristina.kovach@team.wrike.com>
2021-05-08 21:08:12 +0300

    game-calc - add expression generation

---
+++ src/games/game-calc.js
@@ -0,0 +1,26 @@
+import getRandomNumber from '../utils.js';
+
+const minArgNumber = 1;
+const maxArgNumber = 100;
+const mathOperators = ['+', '-', '='];
+const mathOperations = [
+  (arg1, arg2) => arg1 + arg2,
+  (arg1, arg2) => arg1 - arg2,
+  (arg1, arg2) => arg1 * arg2,
+];
+
+const generateExpression = () => {
+  const arg1 = getRandomNumber(minArgNumber, maxArgNumber);
+  const arg2 = getRandomNumber(minArgNumber, maxArgNumber);
+  const indexOfOperation = getRandomNumber(0, mathOperators.length - 1);
+  const operationSymbol = mathOperators[indexOfOperation];
+  const expression = `${arg1} ${operationSymbol} ${arg2}`;
+  const getExpressionResult = mathOperations[indexOfOperation];
+  const result = getExpressionResult(arg1, arg2);
+
+  return [expression, result];
+};
+
+const setGameCalcData = () => {
+  const gameRules = 'What is the result of the expression?';
+};
