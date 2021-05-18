6702d94869cda8953115e550d2fe8ac2a64525b8
Kristina Kovach <kristina.kovach@team.wrike.com>
2021-05-18 22:31:30 +0300

    game-progression - add logic

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env nodе
+import playGame from '../src/index.js';
+import setGamePregressionData from '../src/games/game-progression.js';
+
+playGame(setGamePregressionData);
---
+++ src/games/game-progression.js
@@ -0,0 +1,30 @@
+import generateRandomNumber from '../utils.js';
+
+const generateProgression = () => {
+  const progressionLength = generateRandomNumber(5, 10);
+  const commonDifference = generateRandomNumber(2, 30);
+  const firstElement = generateRandomNumber(1, 10)
+  const hiddenElementPosition = generateRandomNumber(0, progressionLength - 1);
+  const placeholder = '..';
+  const progression = [firstElement];
+
+  for (let i = 0; i < progressionLength - 1; i += 1) {
+    const newElement = progression[i] + commonDifference;
+    progression.push(newElement);
+  }
+
+  const hiddenElement = progression[hiddenElementPosition].toString();
+  progression[hiddenElementPosition] = placeholder;
+
+  return [progression, hiddenElement];
+};
+
+const setGameProgressionData = () => {
+  const gameRules = 'What number is missing in the progression?';
+  const [progression, correctAnswer] = generateProgression();
+  const question = `Question: ${progression.join(' ')}`;
+
+  return [gameRules, question, correctAnswer];
+};
+
+export default setGameProgressionData;
