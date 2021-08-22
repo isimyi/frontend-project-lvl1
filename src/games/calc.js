44369ce8260d95ede67625a8b410d391b5d79fdc
Kristina Kovach <kristina.kovach@team.wrike.com>
2021-08-22 15:23:29 +0300

    calc: change operations structure

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,27 +1,26 @@
 import generateRandomNumber from '../utils.js';
 import playGame from '../index.js';
 
-const minArgNumber = 1;
-const maxArgNumber = 100;
-const mathOperators = ['+', '-', '*'];
-const mathOperations = [
-  (arg1, arg2) => arg1 + arg2,
-  (arg1, arg2) => arg1 - arg2,
-  (arg1, arg2) => arg1 * arg2,
-];
 const gameRules = 'What is the result of the expression?';
+const minOperandNumber = 1;
+const maxOperandNumber = 100;
+const mathOperations = [
+  { '+': (operand1, operand2) => operand1 + operand2 },
+  { '-': (operand1, operand2) => operand1 - operand2 },
+  { '*': (operand1, operand2) => operand1 * operand2 },
+];
 
 const generateExpression = (operand1, operand2, operationSymbol) => `${operand1} ${operationSymbol} ${operand2}`;
 
 const setGameCalcData = () => {
-  const operand1 = generateRandomNumber(minArgNumber, maxArgNumber);
-  const operand2 = generateRandomNumber(minArgNumber, maxArgNumber);
-  const indexOfOperation = generateRandomNumber(0, mathOperators.length - 1);
-  const operationSymbol = mathOperators[indexOfOperation];
+  const operand1 = generateRandomNumber(minOperandNumber, maxOperandNumber);
+  const operand2 = generateRandomNumber(minOperandNumber, maxOperandNumber);
+  const indexOfOperation = generateRandomNumber(0, mathOperations.length - 1);
+  const operationSymbol = Object.keys(mathOperations[indexOfOperation]);
   const expression = generateExpression(operand1, operand2, operationSymbol);
+  const calculateExpressionResult = mathOperations[indexOfOperation][operationSymbol];
 
-  const getExpressionResult = mathOperations[indexOfOperation];
-  const expressionResult = getExpressionResult(operand1, operand2).toString();
+  const expressionResult = calculateExpressionResult(operand1, operand2).toString();
 
   const question = `Question: ${expression}`;
 
