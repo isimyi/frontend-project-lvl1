2ac0d9a590ec767884fb527341d97946bbf9aa6f
Kristina Kovach <kristina.kovach@team.wrike.com>
2021-08-22 13:34:22 +0300

    progression: make generate and hide pure functions

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,39 +3,40 @@ import playGame from '../index.js';
 
 const gameRules = 'What number is missing in the progression?';
 
-const generateProgression = () => {
-  const progressionLength = generateRandomNumber(5, 10);
-  const commonDifference = generateRandomNumber(2, 30);
-  const firstElement = generateRandomNumber(1, 10);
+const generateProgression = (startElement, length, step) => {
   const progression = [];
 
-  for (let i = 0; i < progressionLength - 1; i += 1) {
+  for (let i = 0; i < length - 1; i += 1) {
     if (progression.length === 0) {
-      progression.push(firstElement);
+      progression.push(startElement);
     }
 
-    const newElement = progression[i] + commonDifference;
+    const newElement = progression[i] + step;
     progression.push(newElement);
   }
 
   return progression;
 };
 
-const hideProgressionElement = (setProgression) => {
+const hideProgressionElement = (progression, index) => {
   const placeholder = '..';
-  const resultArray = setProgression();
-  const resultArrayLength = resultArray.length;
-  const hiddenElementPosition = generateRandomNumber(0, resultArrayLength - 1);
-  const hiddenElement = resultArray[hiddenElementPosition].toString();
+  const resultArray = progression;
+  const hiddenElement = resultArray[index].toString();
 
-  resultArray[hiddenElementPosition] = placeholder;
+  resultArray[index] = placeholder;
 
   return [resultArray, hiddenElement];
 };
 
 const setGameProgressionData = () => {
-  const [progression, correctAnswer] = hideProgressionElement(generateProgression);
-  const question = `Question: ${progression.join(' ')}`;
+  const firstMember = generateRandomNumber(1, 10);
+  const progressionLength = generateRandomNumber(5, 10);
+  const commonDifference = generateRandomNumber(2, 30);
+  const hiddenElementPosition = generateRandomNumber(0, progressionLength - 1);
+
+  const progression = generateProgression(firstMember, progressionLength, commonDifference);
+  const [hiddenItemProgression, correctAnswer] = hideProgressionElement(progression, hiddenElementPosition);
+  const question = `Question: ${hiddenItemProgression.join(' ')}`;
 
   return [question, correctAnswer];
 };
