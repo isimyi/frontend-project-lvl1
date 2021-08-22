bd629d1cf1a3822b950aaf1b91afd7acf6653c33
Kristina Kovach <kristina.kovach@team.wrike.com>
2021-08-22 15:25:12 +0300

    calc: remove generateExpression function

--- src/games/calc.js
+++ src/games/calc.js
@@ -10,14 +10,12 @@ const mathOperations = [
   { '*': (operand1, operand2) => operand1 * operand2 },
 ];
 
-const generateExpression = (operand1, operand2, operationSymbol) => `${operand1} ${operationSymbol} ${operand2}`;
-
 const setGameCalcData = () => {
   const operand1 = generateRandomNumber(minOperandNumber, maxOperandNumber);
   const operand2 = generateRandomNumber(minOperandNumber, maxOperandNumber);
   const indexOfOperation = generateRandomNumber(0, mathOperations.length - 1);
   const operationSymbol = Object.keys(mathOperations[indexOfOperation]);
-  const expression = generateExpression(operand1, operand2, operationSymbol);
+  const expression = `${operand1} ${operationSymbol} ${operand2}`;
   const calculateExpressionResult = mathOperations[indexOfOperation][operationSymbol];
 
   const expressionResult = calculateExpressionResult(operand1, operand2).toString();
